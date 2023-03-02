c75c4811810777760c5d45b8d7e846b25eb6f302
viwerd <126782006+viwerd@users.noreply.github.com>
2023-03-02 19:13:53 +0600

    Add files via upload

---
+++ lw2_ex1/lw2_age.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    setlocale(LC_ALL, "ru");
+
+    // stage 1
+    cout << "Сколько лет? ";
+    cin >> age;
+    cout << "Твой возраст - " << age << endl;
+
+    // stage 2
+    if (age < 18 and age > 0)
+    {
+        cout << "Вы еще не достигли совершеннолетия";
+    }
+    
+    // stage 3
+    else if (age >= 18 and age < 30)
+    {
+        cout << "Вы в самом расцвете сил";
+    }
+
+    // stage 4
+    else if (age >= 30 and age < 60)
+    {
+        cout << "Вы находитесь в среднем возрасте»";
+    }
+
+    // stage 5
+    else 
+    {
+        cout << "Вы пожилой гражданин";
+    }
+    return 0;
+}
---
+++ lw2_ex1/lw2_temp1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double F;
+    double C;
+    setlocale(LC_ALL, "ru");
+    
+    cout << "Введите температуру в градусах Фаренгейта: \n";
+    cin >> F;
+
+    if (F <= 32)
+    {
+        cout << "Температура ниже точки замерзания" << endl;
+    }
+    else if (F > 32)
+    {
+        C = (F - 32) * (5.0 / 9.0);
+        cout << "Температура в градусах Цельсия: " << C;
+    }
+    
+}
